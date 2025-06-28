bbf5a3e278eef2073da1dc82ba9ed6add4adf836
Brian Munyoki <Brianmunyoki168@gmail.com>
2025-06-28 15:31:42 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let message=[];
+
+function writeCards(stringNames,eventName){
+for(let i=0;i<stringNames.length ;i++){
+  message.push(`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`)
+}
+  return message
+}
+
+function countDown(number){
+for(let i=number;i>=0;i--){
+  console.log(i)
+}
+return countDown
+}
